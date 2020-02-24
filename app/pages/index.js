6aff120b5340006d391c42d50444c19cca3df49d
MASA-JAPAN <masaifactoryline@gmail.com>
2020-02-24 17:12:05 +0900

    add _app.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,56 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import Navbar from "../components/navbar";
+
 import SideMenu from "../components/sideManu";
 import Carousel from "../components/carousel";
 import MovieList from "../components/movieList";
-import Footer from "../components/footer";
 
 import { getMovies } from "../actions/index";
 
-const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [count, setCount] = useState([]);
-
-  useEffect(async () => {
-    // getMovies().then(movies => {
-    //   setMovies(movies);
-    // });
-    const resMovies = await getMovies();
-    setMovies(resMovies);
-  }, [count]);
-
+const Home = props => {
   return (
     <div>
-      <Head>
-        <title>Home</title>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
-        />
-        <script
-          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossorigin="anonymous"
-        ></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-          crossorigin="anonymous"
-        ></script>
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-          crossorigin="anonymous"
-        ></script>
-      </Head>
-
-      <Navbar />
       <div className="home-page">
         <div className="container">
-          <button onClick={() => setCount(count)}>Click Me!</button>
           <div className="row">
             <div className="col-lg-3">
               <SideMenu appName="Movie DB" />
@@ -60,23 +20,21 @@ const Home = () => {
               <Carousel />
 
               <div className="row">
-                <MovieList movies={movies} />
+                <MovieList movies={props.movies || []} />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Footer />
-
-      <style jsx>
-        {`
-          .home-page {
-            padding-top: 80px;
-          }
-        `}
-      </style>
     </div>
   );
+};
+
+Home.getInitialProps = async () => {
+  const movies = await getMovies();
+  console.log(movies);
+
+  return { movies };
 };
 
 export default Home;
